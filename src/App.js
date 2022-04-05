55f533efbf96184f4cd7638bd4b32c0c62480694
dashratikanta2001 <ratikantadash2001@gmail.com>
2022-04-05 17:25:10 +0530

    6th commit(Fav icon and react-route-dom implement)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React ,{useState}from 'react';
 import Alert from './components/Alert';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [Mode, setMode] = useState('light')// whether dark mode is enable or not
@@ -26,23 +31,28 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor='#202124';
       showAlert("Dark mode has been enabled","success");
+      document.title="Textutils - Dark Mode";
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
       showAlert("Light mode has been enabled","success");
+      document.title="Textutils - Light Mode";
+
     }
   }
   // Dark mode toggle end
   return (
-    <>
+    <Router>
        <Navbar title = {"TextUtils"} mode = {Mode} toggleMode= {toggleMode}/>
        <Alert alert = {alert}/>
        <div className="container my-3">
-         {/* <About/> */}
-       <TextForm  showAlert ={showAlert} heading = "Enter the text to analyze below" mode = {Mode}/>
+       <Routes>
+          <Route exact path="/about" element={<About/>}/>   
+          <Route exact path="/" element={<TextForm  showAlert ={showAlert} heading = "Enter the text to analyze below" mode = {Mode}/>}/>  
+        </Routes>
        </div>
-    </>
+    </Router>
   );
 }
 
